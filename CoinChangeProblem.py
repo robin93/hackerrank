7461b142de9fdc490e8e68afb0e58d8546ba1a26
Singh <robsing@9801a79d5003.ant.amazon.com>
2018-05-26 14:42:02 +0530

    Added comments to explain the edge conditions

--- CoinChangeProblem.py
+++ CoinChangeProblem.py
@@ -8,13 +8,17 @@ import sys
 
 # Complete the getWays function below.
 def getWays(n, c, memo):
+    #if n is 0, then the combination of elements used is valid and hence will count towards the number of ways
     if n ==0:
         return 1
+    #if n is less than 0, then the combination of elements used is not valid and will not count towards the ways
     elif n < 0:
         return 0
+    #c[1:0] for list of one element will return an empty list and no possible ways from this combination
     elif len(c)==0:
         return 0
     
+    #Create a cache with key uniquely represting the n and list c
     key = (tuple(c),n)
     if key in memo:
         return memo[key]
@@ -26,13 +30,8 @@ def getWays(n, c, memo):
 if __name__ == '__main__':
 
     nm = input().split()
-
-    n = int(nm[0])
-
-    m = int(nm[1])
-
+    n,m = int(nm[0]),int(nm[1])
     c = list(map(int, input().rstrip().split()))
-
     # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
     
     memo = {}
